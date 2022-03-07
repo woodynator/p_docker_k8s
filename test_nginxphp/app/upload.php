60e215b015a41f9f65f8c2bda152305d04b4c62d
Jonatan Madera <jonamade2@gmail.com>
2022-03-07 15:56:03 +0100

    some more changes

--- test_nginxphp/app/upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-if(!empty($_FILES['uploaded_file'])) {
-    \move_uploaded_file($_FILES['uploaded_file']['tmp_name'], '/app/uploads/image.jpg');
-}
-
-header('Location: /');
